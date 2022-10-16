dfc81dcae168554742068af223ec0931ebad0c93
FernandoMichea90 <fmicheam@gmail.com>
2022-10-16 04:27:08 -0300

    cambios

---
+++ doli_html/install/install.forced.php
@@ -0,0 +1,21 @@
+<?php
+// Forced install config file for Dolibarr 15.0.2 (Sun Oct 16 09:06:28 CEST 2022)
+$force_install_nophpinfo = true;
+$force_install_noedit = 2;
+$force_install_message = 'Dolibarr installation';
+$force_install_main_data_root = '/var/www/documents';
+$force_install_mainforcehttps = !empty('0');
+$force_install_database = 'dolibarr';
+$force_install_type = 'mysqli';
+$force_install_dbserver = 'mariadb';
+$force_install_port = '3306';
+$force_install_prefix = 'llx_';
+$force_install_databaselogin = 'dolibarr';
+$force_install_databasepass = 'dolibarr';
+$force_install_createuser = !empty('');
+$force_install_createdatabase = !empty('');
+$force_install_databaserootlogin = '';
+$force_install_databaserootpass = '';
+$force_install_dolibarrlogin = 'admin';
+$force_install_lockinstall = true;
+$force_install_module = '';
